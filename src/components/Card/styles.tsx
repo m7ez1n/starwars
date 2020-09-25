b5cb27d9edc0334b9cd076cf602c59e1e11ba19e
Mateus Mendes <mateuspm2015@hotmail.com>
2020-09-25 10:02:06 -0300

    definindo as propriedades de cada categoria

--- src/components/Card/styles.tsx
+++ src/components/Card/styles.tsx
@@ -5,3 +5,29 @@ export const Wrapper = styled.section`
   height: 50px;
   background-color: red;
 `;
+
+ /**
+  *  ---> Pessoas <---
+  *  - Nome
+  *  - Altura
+  *  - Genêro 
+  *  - Ano de nascimento
+  *  
+  * ---> Filmes <---
+  *   - titulo 
+  *   - diretor 
+  *   - data de lançamento
+  *   - episodio id
+  *
+  * ---> Veiculos <---
+  *   - Name
+  *   - Model
+  *   - manufacturer
+  *   - vehicle_class
+  * 
+  * ---> Species <---
+  *   -Name
+  *   - language
+  *   -classification
+  *   - skin_colors
+  * */
