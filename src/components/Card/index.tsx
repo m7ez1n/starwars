9e11b314e1acd38b57cb8a060580ee9d50917b89
m7he4rt <mateuspm2015@hotmail.com>
2020-09-26 07:23:52 -0300

    feat: styling card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,9 +1,32 @@
 import * as S from './styles';
 
-export const Menu = () => {
+export type CardProps = {
+  name?: string;
+  height?: string;
+  gender?: string;
+  birth_year?: string;
+};
+
+export const Card = ({ name, height, gender, birth_year }: CardProps) => {
   return (
-    <S.Wrapper>
-      <h1>Role dms</h1>  
-    </S.Wrapper>
+    <S.wrapper>
+      <S.content>
+        <S.name>{name}</S.name>
+        <S.wrapperCharacteristic>
+          <span>Height</span>
+          <p>{height}</p>
+        </S.wrapperCharacteristic>
+
+        <S.wrapperCharacteristic>
+          <span>Gender</span>
+          <p>{gender}</p>
+        </S.wrapperCharacteristic>
+
+        <S.wrapperCharacteristic>
+          <span>Birth year</span>
+          <p>{birth_year}</p>
+        </S.wrapperCharacteristic>
+      </S.content>
+    </S.wrapper>
   );
-}
+};
--- src/components/Card/styles.tsx
+++ src/components/Card/styles.tsx
@@ -1,33 +1,76 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.section`
-  width: 50px;
-  height: 50px;
-  background-color: red;
+export const wrapper = styled.section`
+  margin-top: 20px;
+  margin-left: 40px;
+  max-height: 250px;
+  min-height: 200px;
+  max-width: 400px;
+  min-width: 200px;
+  width: auto;
+  height: auto;
+
+  border-radius: 20px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+
+  &:hover {
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  }
 `;
 
- /**
-  *  ---> Pessoas <---
-  *  - Nome
-  *  - Altura
-  *  - Genêro 
-  *  - Ano de nascimento
-  *  
-  * ---> Filmes <---
-  *   - titulo 
-  *   - diretor 
-  *   - data de lançamento
-  *   - episodio id
-  *
-  * ---> Veiculos <---
-  *   - Name
-  *   - Model
-  *   - manufacturer
-  *   - vehicle_class
-  * 
-  * ---> Species <---
-  *   -Name
-  *   - language
-  *   -classification
-  *   - skin_colors
-  * */
+export const content = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+export const name = styled.h4`
+  text-transform: uppercase;
+  letter-spacing: 3px;
+  margin-bottom: 15px;
+`;
+
+export const wrapperCharacteristic = styled.article`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  margin-bottom: 15px;
+
+  span {
+    font-size: 16px;
+    color: #4e4e4e;
+  }
+
+  p {
+    font-size: 20px;
+    color: black;
+  }
+`;
+
+/**
+ *  ---> Pessoas <---
+ *  - Nome
+ *  - Altura
+ *  - Genêro
+ *  - Ano de nascimento
+ *
+ * ---> Filmes <---
+ *   - titulo
+ *   - diretor
+ *   - data de lançamento
+ *   - episodio id
+ *
+ * ---> Veiculos <---
+ *   - Name
+ *   - Model
+ *   - manufacturer
+ *   - vehicle_class
+ *
+ * ---> Species <---
+ *   -Name
+ *   - language
+ *   -classification
+ *   - average_lifespan
+ * */
